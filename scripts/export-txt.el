fe0075e2133f97a8ba3b6d179f619c8fadea561d
Glenn Barry <gaak99@gaak99.com>
2018-09-04 20:09:30 -0400

    initial commit (org2txt)

---
+++ scripts/export-txt.el
@@ -0,0 +1,17 @@
+;;; q&d ox-pandoc "script" for remacs --batch --script
+;;  sep18, xps13
+
+(let ((myorgf "~/Desktop/tech/find-job/spring18/resume-gh/gb-res/gb-resume.org")
+      (dash_path "/home/gb/.emacs.d/elpa/dash-2.13.0/")
+      (ht_path "~/.emacs.d/elpa/ht-2.2/")
+      (ox_pandoc_path "~/warez/emacs-lisp/ox-pandoc/"))
+  (setq load-path
+	(cons (expand-file-name dash_path) load-path))
+  (setq load-path
+	(cons (expand-file-name ht_path) load-path))
+  (setq load-path
+	(cons (expand-file-name ox_pandoc_path) load-path))
+  (load-library "ox-pandoc")
+  (find-file myorgf)
+  ;(org-pandoc-export-as-asciidoc)
+  (org-pandoc-export-to-asciidoc))
