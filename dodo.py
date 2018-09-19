c48d249a9ba35f91951f72fd630d60db4426f7fa
Glenn Barry <gaak99@gaak99.com>
2018-09-19 15:13:07 -0400

    add debugmemaybe gloval var/flag

--- dodo.py
+++ dodo.py
@@ -5,6 +5,8 @@
 #     'verbosity': 2
 # }
 
+debugmemaybe=False
+
 res_dir="/home/gb/Desktop/tech/find-job/spring18/resume-gh/gb-res"
 res_base_f="gb-resume"
 res_org_f=res_dir + "/" + res_base_f + ".org"
@@ -20,7 +22,8 @@ git_push='git push origin master'
 git_add_html="git add " + res_base_f + ".html" + ' ' + res_base_f + '.org'
 git_commit_html="git commit -m doit_auto_update " + res_base_f + ".html" + ' ' + res_base_f + '.org'
 full_cmd_org2html=cmd_org2html + " && " + git_add_html + " && " + git_commit_html  + '&&' + git_push
-print("gbdebug_full_org2html: %s" % full_cmd_org2html)
+if debugmemaybe:
+    print("gbdebug_full_org2html: %s" % full_cmd_org2html)
 
 res_txt_f=res_dir + "/" + res_base_f + ".txt"
 export_txt_el="scripts/export-txt.el"
@@ -29,7 +32,8 @@ cmd_org2txt=remacs + " " + emacs_opts_common + " " + "--script" + " " + export_t
 git_add_txt="git add " + res_base_f + ".txt"
 git_commit_txt="git commit -m doit_auto_update " + res_base_f + ".txt"
 full_cmd_org2txt=cmd_org2txt + " && " + git_add_txt + " && " + git_commit_txt + '&&' + git_push
-print("gbdebug_full_org2txt: %s" % full_cmd_org2txt)
+if debugmemaybe:
+    print("gbdebug_full_org2txt: %s" % full_cmd_org2txt)
 
 def task_res_org2html():
     return {'actions': [full_cmd_org2html],
@@ -51,7 +55,8 @@ cmd_io_update='cp ' + res_html_f + ' ' + io_f
 git_add_io='git add ' + io_base_f
 git_commit_io='git commit -m doit_auto_update ' + io_base_f
 full_cmd_io_update=cmd_io_update + '&&' + 'cd ' + io_dir + '&&' + git_add_io + '&&' + git_commit_io  + '&&' + git_push
-print("gbdebug_full_cmd_io_update: %s" % full_cmd_io_update)
+if debugmemaybe:
+    print("gbdebug_full_cmd_io_update: %s" % full_cmd_io_update)
 
 def task_res_io_update():
     return {'actions': [full_cmd_io_update],
